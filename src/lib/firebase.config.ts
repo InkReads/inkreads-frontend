5c619c938d56f303cab59d2f5cd10d303ecb392f
oofmar <omar.moralesrunof2005@gmail.com>
2025-02-09 21:22:52 -0800

    Added user profile saving

--- src/lib/firebase.config.ts
+++ src/lib/firebase.config.ts
@@ -11,13 +11,13 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+	apiKey: "AIzaSyBtnhMy10t1v8NTuB48Bfcuf8y8WHlLAYg",
+	authDomain: "inkreads-a9b7e.firebaseapp.com",
+	projectId: "inkreads-a9b7e",
+	storageBucket: "inkreads-a9b7e.firebasestorage.app",
+	messagingSenderId: "74535854826",
+	appId: "1:74535854826:web:0c397ba589b0a849b3ed93",
+	measurementId: "G-VMCSFVQ6MX"
 };
 
 // Initialize Firebase
